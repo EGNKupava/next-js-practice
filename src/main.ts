19aab74f11671126783c66cce4e98023fba037b7
y.kupava <y.kupava@godeltech.com>
2023-03-30 11:27:03 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const options = new DocumentBuilder().setTitle('API').build();
+
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [AppModule],
+  });
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
